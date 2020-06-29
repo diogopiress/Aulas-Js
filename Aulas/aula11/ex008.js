de465caefb830cbe8e59ff958db79cff713c4a49
Digo Pires <67446406+diogopiress@users.noreply.github.com>
2020-06-28 22:20:23 -0300

    aula 11

--- Aulas/aula11/ex008.js
+++ Aulas/aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('o console funcionaou corretamente')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h `)
+if (vel >60){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitada. MULTADO!!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança` )
---
+++ Aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'USA'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil'){
+    console.log('Você é estrangeiro!')
+}else{
+    console.log('Voce é Brasileiro!')
+}
