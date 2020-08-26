d19e697945fc98f17b1d35ee566c8040385f23b8
diogopiress <67446406+diogopiress@users.noreply.github.com>
2020-08-26 00:46:08 -0300

    aula 12 exercio 13 incio
    ta dando erro no checke no java script

---
+++ Aulas/aula012ex/ex015/script.js
@@ -0,0 +1,24 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    
+    if(fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByTagName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'homem'
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+
+
+        // res.innerHTML = `calalld ${idade}`
+    }
+
+}
