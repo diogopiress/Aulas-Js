f707d5c9d053d1420314ebd76249a0684a74f2a3
Diogo Pires <67446406+diogopiress@users.noreply.github.com>
2020-09-22 22:57:18 -0300

    termino da aula 12 exercios

--- Aulas/aula012ex/ex015/script.js
+++ Aulas/aula012ex/ex015/script.js
@@ -7,17 +7,46 @@ function verificar() {
     if(fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByTagName('radsex')
+
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gênero = ''
+        var genero = ''
+        var img = document.createElement('img') /*cria uma tag pelo JavaScript*/
+        img.setAttribute('id', 'foto') /*criando um id com nome foto para a tag img*/
         if (fsex[0].checked) {
-            gênero = 'homem'
+            genero = 'homem'
+            if (idade >= 0 && idade < 11){
+                //criança
+                img.setAttribute('src', 'imagem/bebe-homem.png')
+            } else if (idade < 22){
+                //Jovem
+                img.setAttribute('src', 'imagem/jovem-homem.png')
+            } else if (idade < 51){
+                //Adulto
+                img.setAttribute('src', 'imagem/adulto-homem.png')
+            } else{
+                //idoso
+                img.setAttribute('src', 'imagem/idoso-homem.png')
+            }
         } else if (fsex[1].checked) {
-            gênero = 'Mulher'
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 11) {
+                //criança
+                img.setAttribute('src', 'imagem/bebe-mulher.png')
+            } else if (idade < 22) {
+                //jovem
+                img.setAttribute('src', 'imagem/jovem-mulher.png')
+            } else if (idade < 51){
+                //Adulto
+                img.setAttribute('src', 'imagem/adulto-mulher.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagem/idoso-mulher.png')
+            }
         }
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
-
-
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img) //adicionando a imagem no body do html
         // res.innerHTML = `calalld ${idade}`
     }
 
